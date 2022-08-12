4d03eb0d333e8ad7d1d6a3472de04732ae3954dc
ameyalande <lande260@gmail.com>
2022-08-12 10:24:56 +0530

    d

--- src/main/java/com/organicstore/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.organicstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.organicstore.model.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
